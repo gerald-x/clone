f9ae3953ed6002e47ea2e0c293e72d9fad0397cf
KingFrankostein <frankostein96@gmail.com>
2021-04-10 14:18:04 +0100

    started dashboard design

---
+++ account/index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Binance</title>
+    <link rel="stylesheet" href="css/main.css"/>
+    <link rel="stylesheet" href="css/sidebar.css"/>
+    <link rel="stylesheet" href="css/content.css"/>
+</head>
+<body>
+
+
+    <div class="sidebar">
+<ul>
+<li><a href="#">Overview</a></li>
+<li><a href="#">Fiat and Spot</a></li>
+<li><a href="#">Margins</a></li>
+<li><a href="#">Futures</a></li>
+<li><a href="#">P2P</a></li>
+<li><a href="#">Earn</a></li>
+<li><a href="#">Pool</a></li>
+<li><a href="#">Vanilla Options</a></li>
+<li><a href="#">BPLAY</a></li>
+<li><a href="#">Binance TR</a></li>
+<li><a href="#">Transaction History</a></li>
+
+</ul>
+    
+    </div>
+
+
+    <div class="content-wrap">
+<div class="header-box">
+
+<div class="title">
+    Dashboard
+</div>
+
+
+<div class="btn-group">
+<a href="#" class="gold-btn">Deposit</a>
+<a href="#" class="transp-btn">Withdraw</a>
+
+</div>
+</div>
+
+
+
+
+<div class="balance-box">
+<div class="name">
+Estimated Balance
+
+</div>
+
+<div class="amount">
+<div class="main">0.00000000</div>
+<div class="crypt">BTC</div>    
+<div class="equiv">≈ $0.000000</div>
+</div>
+
+
+</div>
+
+<div class="extra-box">
+    <div class="name">
+    Estimated Balance
+    
+    </div>
+    
+    <div class="amount">
+    <div class="main">0.00000000</div>
+    </div>
+    
+    
+    </div>
+
+    </div>
+    
+</body>
+</html>
